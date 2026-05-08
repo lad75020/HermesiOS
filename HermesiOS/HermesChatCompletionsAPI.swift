d866f0d2b492bd3bcef03be8702c23417b895ed3
ladparis <laurent@dubertrand.fr>
2026-05-08 23:02:33 +0200

    Encode chat stream flag
    Make Chat Completions request body explicitly encode the stream Boolean so Hermes Agent receives true or false with every request.

--- HermesiOS/HermesChatCompletionsAPI.swift
+++ HermesiOS/HermesChatCompletionsAPI.swift
@@ -726,6 +726,21 @@ private struct HermesChatCompletionsRequestBody: Encodable {
     let messages: [HermesChatRequestMessage]
     let stream: Bool
     let user: String?
+
+    enum CodingKeys: String, CodingKey {
+        case model
+        case messages
+        case stream
+        case user
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(model, forKey: .model)
+        try container.encode(messages, forKey: .messages)
+        try container.encode(stream, forKey: .stream)
+        try container.encodeIfPresent(user, forKey: .user)
+    }
 }
 
 private enum HermesChatMessageContentPayload: Encodable {
